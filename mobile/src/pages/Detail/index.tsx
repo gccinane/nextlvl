bff987fd87dd88881a7fff24070242f1da101dfc
Jose <gccinane@gmail.com>
2020-06-05 19:23:26 -0300

    detail screen finished

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView, Linking } from 'react-native';
 import {RectButton} from 'react-native-gesture-handler'
-import {useNavigation} from '@react-navigation/native'
+import {useNavigation, useRoute} from '@react-navigation/native'
 import {Feather as Icon, FontAwesome} from '@expo/vector-icons'
+import api from '../../services/api'
+import * as MailComposer from 'expo-mail-composer';
 
 
 
+interface Params{
+  point_id: number;
+}
 
-const Detail: React.FC = () => {
+interface Data {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  }
+  items: {  
+    title:string;
+  }[],
+}
+
+const Detail= () => {
   const navigation = useNavigation()
+  const route = useRoute()
+  const routeParams  = route.params as Params
+  const [data, setData] = useState<Data>({} as Data) 
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(res => {
+      setData(res.data)
+    })
+  },[])
 
   function handleNavigateBack(){
     navigation.goBack();
   }
-  
+
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+
+    })
+  }
+
+  function handleWhatsapp(){
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse`)
+  }
+
+  if(!data.point) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={{flex:1}}> 
       <View style={styles.container}>
@@ -21,22 +65,22 @@ const Detail: React.FC = () => {
           <Icon name = "arrow-left" size = {20} color = "#34cb79" onPress= {handleNavigateBack}/>
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{uri: 'https://i.pinimg.com/236x/ca/76/0b/ca760b70976b52578da88e06973af542.jpg'}}/>
-        <Text style={styles.pointName}>Mercadão</Text>
-        <Text style={styles.pointItems}>Lampadas oleo de cozinha</Text>
+        <Image style={styles.pointImage} source={{uri: data.point.image}}/>
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
         <View style={styles.address}>
-          <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>RIo do cul</Text>
+          <Text style={styles.addressTitle}>{data.point.city}</Text>
+          <Text style={styles.addressContent}>{data.point.uf}</Text>
         </View>
 
       </View>
       <View style={styles.footer}>
-        <RectButton style={styles.button} >
+        <RectButton style={styles.button} onPress={handleWhatsapp} >
           <FontAwesome name="whatsapp" size={20} color="#fff"/>
           <Text style={styles.buttonText}>Whatsapp</Text>
         </RectButton>
-        <RectButton style={styles.button} >
+        <RectButton style={styles.button} onPress= {handleComposeMail}>
           <Icon name="mail" size={20} color="#fff"/>
           <Text style={styles.buttonText}>Email</Text>
         </RectButton>
@@ -49,7 +93,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20 ,
+    paddingTop: 45,
   },
 
   pointImage: {
@@ -96,6 +140,7 @@ const styles = StyleSheet.create({
     borderTopWidth: StyleSheet.hairlineWidth,
     borderColor: '#999',
     paddingVertical: 20,
+
     paddingHorizontal: 32,
     flexDirection: 'row',
     justifyContent: 'space-between'
